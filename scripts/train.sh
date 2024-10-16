5eafd28f4e2770298fa26ce892f6a3b4a3e77d8b
LLLiuxk <lxk811@gmail.com>
2024-10-16 15:06:41 +0800

    1016_2

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export RESULT_FOLDER="/mnt/data/shared/lxk/BDR_diffusion/results/model"
+export RESULT_FOLDER="/mnt/data/shared/lxk/BDR_diffusion/results/"
 /mnt/data/shared/lxk/miniconda3/envs/bdr_diffusion/bin/python train.py --results_folder $RESULT_FOLDER --data_class microstructure --name model --batch_size 128 --new True --continue_training False --image_size 128 --training_epoch 2000 --ema_rate 0.999 --base_channels 32 --save_last False --save_every_epoch 500 --with_attention True  --split_dataset False  --lr 1e-4 --optimizier adamw --sdf_folder /home/fjx/bdr6/
